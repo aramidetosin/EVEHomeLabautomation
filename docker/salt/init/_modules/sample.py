13dd943f3600e865767dad18576b270e6a430961
aramidetosin <aoluwatosin10@gmail.com>
2020-04-19 19:14:02 +0100

    testing extended modules

--- docker/salt/init/_modules/sample.py
+++ docker/salt/init/_modules/sample.py
@@ -14,12 +14,8 @@ def third():
 
 
 def users():
-    regex_users = r"user\s(?P<user>\S+)\s+\{"
-    result = __salt__["napalm.netmiko_commands"](
+    pattern = re.compile(r"user")
+    res = __salt__["napalm.netmiko_commands"](
         "show configuration system login")
-    matches = re.finditer(regex_users, result, re.MULTILINE)
-    for matchNum, match in enumerate(matches, start=1):
-        for groupNum in range(0, len(match.groups())):
-            groupNum = groupNum + 1
-            group = match.group(groupNum)
-            return group
+    matches = pattern.finditer(res)
+    return matches
