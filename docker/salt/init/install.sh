105efa787c644f9866848b22fd61e89d0245cf0f
aramidetosin <aoluwatosin10@gmail.com>
2020-04-22 14:03:45 +0100

    install script for ubuntu server

--- docker/salt/init/install.sh
+++ docker/salt/init/install.sh
@@ -1,14 +1,14 @@
-sudo apt-get -qq update apt-get -y install apt-utils sudo wget openssh-server python3 gnupg git iputils-ping net-tools traceroute mtr vim python3-pip tree  && rm -rf /var/lib/apt/lists/*
+sudo apt-get update apt-get -y install apt-utils sudo wget openssh-server python3 gnupg git iputils-ping net-tools traceroute mtr vim python3-pip tree  && rm -rf /var/lib/apt/lists/*
 
 wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-touch /etc/apt/sources.list.d/saltstack.list
-echo "deb http://repo.saltstack.com/py3/ubuntu/18.04/amd64/latest bionic main" >> /etc/apt/sources.list.d/saltstack.list
+sudo touch /etc/apt/sources.list.d/saltstack.list
+sudo echo "deb http://repo.saltstack.com/py3/ubuntu/18.04/amd64/latest bionic main" >> /etc/apt/sources.list.d/saltstack.list
 sudo apt-get update && apt-get -y install salt-master salt-minion
-echo "master: 127.0.0.1" >> /etc/salt/minion
-touch /etc/salt/proxy
-echo "master: localhost\npki_dir: /etc/salt/pki/proxy\ncache_dir: /var/cache/salt/proxy\nmultiprocessing: False" >> /etc/salt/proxy
-salt-master -d
-salt-minion -d
+sudo echo "master: 127.0.0.1" >> /etc/salt/minion
+sudo touch /etc/salt/proxy
+sudo echo "master: localhost\npki_dir: /etc/salt/pki/proxy\ncache_dir: /var/cache/salt/proxy\nmultiprocessing: False" >> /etc/salt/proxy
+sudo salt-master -d
+sudo salt-minion -d
 
 sudo pip3 install --upgrade pip
 sudo pip3 install napalm
