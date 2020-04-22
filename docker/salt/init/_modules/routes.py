9693895c689c3062310933724ba4c38dfa39c03b
aramidetosin <aoluwatosin10@gmail.com>
2020-04-22 23:15:53 +0100

    Testing Orchs

--- docker/salt/init/_modules/routes.py
+++ docker/salt/init/_modules/routes.py
@@ -10,8 +10,11 @@ def prefix_active_in_route_table(prefix, **kwargs):
     result_list = []
     if (__grains__["os"] == "ios") or (__grains__["os"] == "junos"):
         res = __salt__["route.show"](prefix)
-        for protocol_list in res["out"][prefix]:
-            result_list.append(protocol_list["current_active"])
+        if prefix in res["out"].keys():
+            for protocol_list in res["out"][prefix]:
+                result_list.append(protocol_list["current_active"])
+        else:
+            return False
     else:
         return (False, "Os is not supported")
     if result_list.count(True) >= 1:
@@ -32,8 +35,11 @@ def prefix_not_active_in_route_table(prefix, **kwargs):
     result_list = []
     if (__grains__["os"] == "ios") or (__grains__["os"] == "junos"):
         res = __salt__["route.show"](prefix)
-        for protocol_list in res["out"][prefix]:
-            result_list.append(protocol_list["current_active"])
+        if prefix in res["out"].keys():
+            for protocol_list in res["out"][prefix]:
+                result_list.append(protocol_list["current_active"])
+        else:
+            return True
     else:
         return (False, "Os is not supported")
     if result_list.count(True) >= 1:
